13bcd2c406a6b3447bad661176adadcb3856991a
jejaksaid <said@fastlearningcenter.com>
2021-01-25 11:33:33 +0700

    js_operators

---
+++ js_operators.js
@@ -0,0 +1,112 @@
+// javascript support arithmetic operators + - * / % ++ --
+
+var a = 33;
+var b = 10;
+var c = "Test";
+var linebreak = "<br />";
+
+document.write("a-b= ");
+result = a - b;
+document.write(result);
+document.write(linebreak);
+
+document.write("a*b= ");
+result = a * b;
+document.write(result);
+document.write(linebreak);
+
+document.write("a/b= ");
+result = a / b;
+document.write(result);
+document.write(linebreak);
+
+document.write("a%b= ");
+result = a % b;
+document.write(result);
+document.write(linebreak);
+
+document.write("a+b+c= ");
+result = a + b +c ;
+document.write(result);
+document.write(linebreak);
+
+document.write("a++b= ");
+result =  ++ b;
+document.write(result);
+document.write(linebreak);
+
+document.write("a--b= ");
+result =  -- b;
+document.write(result);
+document.write(linebreak);
+
+// comparasion operators == != > < >= <=
+var a = 10;
+var b = 20;
+var linebreak = "<br />"
+
+document.write("(a==b) => ");
+result = (a == b );
+document.write(result);
+document.write(linebreak);
+
+document.write("(a!=b) => ");
+result = (a != b );
+document.write(result);
+document.write(linebreak);
+
+document.write("(a>b) => ");
+result = (a > b );
+document.write(result);
+document.write(linebreak);
+
+document.write("(a<b) => ");
+result = (a < b );
+document.write(result);
+document.write(linebreak);
+
+document.write("(a>=b) => ");
+result = (a >= b );
+document.write(result);
+document.write(linebreak);
+
+document.write("(a<=b) => ");
+result = (a <= b );
+document.write(result);
+document.write(linebreak);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
