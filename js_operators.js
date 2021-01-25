2d05c33f2ec23b087be1c01cde5537a3ff6ce558
jejaksaid <said@fastlearningcenter.com>
2021-01-25 15:29:04 +0700

    js_operators

--- js_operators.js
+++ js_operators.js
@@ -1,12 +1,25 @@
+var linebreak = "<br />";
+document.write(linebreak);
+document.write(linebreak);
+
+
 // javascript support arithmetic operators + - * / % ++ --
+document.write("Arithmetic Operators");
+document.write(linebreak);
+
 
 var a = 33;
 var b = 10;
 var c = "Test";
 var linebreak = "<br />";
 
+document.write("A = 33")
+document.write(linebreak);
+document.write("B = 10")
+document.write(linebreak);
+
 document.write("a-b= ");
-result = a - b;
+result = (a - b);
 document.write(result);
 document.write(linebreak);
 
@@ -26,24 +39,38 @@ document.write(result);
 document.write(linebreak);
 
 document.write("a+b+c= ");
-result = a + b +c ;
+result = a + b + c ;
 document.write(result);
 document.write(linebreak);
 
-document.write("a++b= ");
-result =  ++ b;
+a = ++a;
+document.write("++a= ");
+result =  (++a);
 document.write(result);
 document.write(linebreak);
 
-document.write("a--b= ");
-result =  -- b;
+b = --b;
+document.write("--b= ");
+result =  (--b);
 document.write(result);
 document.write(linebreak);
+document.write(linebreak);
+document.write(linebreak);
 
+
+document.write("Comparasion Operators");
 // comparasion operators == != > < >= <=
+document.write(linebreak);
+
 var a = 10;
 var b = 20;
 var linebreak = "<br />"
+
+document.write(" A = 10")
+document.write(linebreak);
+document.write(" B = 20 ")
+
+document.write(linebreak)
 
 document.write("(a==b) => ");
 result = (a == b );
@@ -74,20 +101,49 @@ document.write("(a<=b) => ");
 result = (a <= b );
 document.write(result);
 document.write(linebreak);
+document.write(linebreak);
+document.write(linebreak);
 
 
+document.write("Logical Operators");
+// logical operators && (Logical AND) || (Logical OR) ! (Logical NOT)
+document.write(linebreak);
+
+var a = true;
+var b = false;
+var linebreak = "<br />";
+
+document.write("(a && b => ");
+result = ( a && b );
+document.write(result);
+document.write(linebreak)
+ 
+document.write("(a || b) => ");
+result = (a || b);
+document.write(result);
+document.write(linebreak);
+
+document.write("!(a && b) => ");
+result = (!(a && b));
+document.write(result);
+document.write(linebreak);
+
+document.write('bitwise')
+document.write(linebreak);
+
+var a = 3;
+var b = 5;
+
+document.write("(~b) => ");
+result = (~b);
+document.write(result);
+document.write(linebreak);
 
 
-
-
-
-
-
-
-
-
-
-
+document.write("(a << b) => ");
+result = (a << b);
+document.write(result);
+document.write(linebreak);
 
 
 
