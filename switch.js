9a100fef5e2c0fff001f013dfd5b33fd1182856c
jejaksaid <said@fastlearningcenter.com>
2021-01-26 11:41:28 +0700

    switch break and without break

---
+++ switch.js
@@ -0,0 +1,29 @@
+/* grade = 'C';
+document.write("Entering Switch Block");
+
+switch(grade) {
+    case 'A' : document.write("Good Job");
+    break;
+    case 'B' : document.write('nice');
+    break;
+    case 'C' : document.write('bad');
+    break;
+
+    default: document.write('Unknown grade');
+}
+document.write("exit"); */
+
+grade = 'A';
+document.write("Entering Switch Block");
+
+switch(grade) {
+    case 'A' : document.write("Good Job");
+    
+    case 'B' : document.write('nice');
+    
+    case 'C' : document.write('bad');
+    
+
+    default: document.write('Unknown grade');
+}
+document.write("exit");
