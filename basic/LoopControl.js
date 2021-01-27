386d7ec42f8aab0d713d94d738685ac15dabaf73
jejaksaid <said@fastlearningcenter.com>
2021-01-27 11:03:53 +0700

    continue, break, and label

--- basic/LoopControl.js
+++ basic/LoopControl.js
@@ -1,14 +1,50 @@
-var x = 1;
-var text = "";
+// var x = 1;
+// var text = "";
 
-while (x < 20 ) {
-    x = x + 1;
+// while (x < 20 ) {
+//     x = x + 1;
     
-    if (x == 10 ) {
-        continue; //breaking the loop
-    }
-    text += "<br> The number from break is " + x;
+//     if (x == 10 ) {
+//         continue; //skip the loop
+//     }
+//     text += "<br> The number from break is " + x;
 
         
+// }
+// document.write(text)
+
+var text = "";
+var i;
+
+for (i = 0; i < 10; i++) {
+    if (i == 3) {
+        continue;
+    }
+    text += "The number is " + i + "<br>";    
+}
+document.write(text)
+
+var text = "";
+var i;
+
+for (i = 0; i < 10; i++) {
+    if (i == 4) {
+        break;
+    }
+    text += "The number is " + i + "<br>";
+}
+document.write(text)
+
+// using label
+
+var cars = ["BMW", "Volvo", "Ford", "Chevrolet"];
+var text = "";
+
+list: {
+    text += cars[0] + "<br>";
+    text += cars[1] + "<br>";
+    break list;
+    text += cars[3];
+    text += cars[4];
 }
 document.write(text)
