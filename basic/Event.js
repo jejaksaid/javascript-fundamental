3c9aa2e0225b9a90a7e5928c2091be58215a39a5
jejaksaid <said@fastlearningcenter.com>
2021-01-28 21:57:17 +0700

    Event time, mouseover, mouseout

---
+++ basic/Event.js
@@ -0,0 +1,13 @@
+// mouseover and mouseout
+/*
+function over() {
+    document.write("Mouse Over");
+}
+
+function out() {
+    document.write("Mouse Out");
+} */
+
+function displayDate() {
+    document.getElementById("demo").innerHTML = Date();
+}
