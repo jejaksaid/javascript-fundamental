6b226ba0bb06f03a82d8c37659df915a1a182e3b
jejaksaid <said@fastlearningcenter.com>
2021-02-04 00:48:37 +0700

    Nan, infinity, hexadecimal, number can be object

--- oop/Number.js
+++ oop/Number.js
@@ -9,6 +9,39 @@ document.write(" + : " + x + "<br>");
 document.write(" - : " + y + "<br>");
 */
 
-var x = (0.2 * 10 + 0.1 * 10) / 10;
+// var x = (0.2 * 10 + 0.1 * 10) / 10;
 
-document.write(x);
+// document.write(x);
+
+// var x = 10 ;
+// var y = 20;
+// var z = "\'30'";
+// console.log("result :" + x + y +z)
+
+// Numeric string
+var x = "100";
+var y = "10";
+console.log(x+y);
+
+// Not a Number (NaN)
+var x = NaN;
+var y = "5";
+var z = x - y;
+console.log(z)
+
+// infinity
+var myNumber = 2;
+while (myNumber != Infinity) {
+    myNumber = myNumber * myNumber;
+}
+document.write(myNumber);
+
+// hexadecimal
+var x = 0xFF; 
+console.log(x)
+
+// number can be object
+var x = new Number(123);
+var y = 123;
+console.log(typeof(x));
+console.log(typeof(y));
