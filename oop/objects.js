e39adb0d4a0685f493cb6de369a0f82752447ba9
jejaksaid <said@fastlearningcenter.com>
2021-02-04 00:11:00 +0700

    example 4

--- oop/objects.js
+++ oop/objects.js
@@ -23,6 +23,7 @@ document.write("Book author is : " + myBook.author + "<br>");
 
 
 // define a function which will work as a method 
+/*
 function addPrice(amount) {
     this.price = amount;
 }
@@ -34,8 +35,29 @@ function book(title, author) {
 }
 
 var myBook = new book("Javascript", "saidramadhan");
-myBook.addPrice(100);
+myBook.addPrice(`$ 100`);
 
 document.write("Book title : " + myBook.title + "<br>");
 document.write("Book author : " + myBook.author + "<br>")
 document.write("Book Price : " + myBook.price + "<br>")
+*/
+
+function addPrice(amount) {
+    with(this) {
+        price = amount;
+    }
+}
+
+function book(title, author) {
+    this.title = title;
+    this.author = author;
+    this.price = 0;
+    this.addPrice = addPrice // assign that method as property
+}
+
+var myBook = new book("CSS", "Saidramadhan");
+myBook.addPrice(500);
+
+document.write("Book title : " + myBook.title +  "<br>");
+document.write("Book Author : " + myBook.author + "<br>")
+document.write("Book price : " + myBook.price + "<br>")
