70815a0be3627e2e2b302277d3779a2f1a0702d1
jejaksaid <said@fastlearningcenter.com>
2021-02-04 00:22:19 +0700

    Numbers javascript

---
+++ oop/Number.js
@@ -0,0 +1,14 @@
+/*
+var x = 3.14; // number with decimals
+var y = 3; // number without decimals
+
+var x = 123e5; // exponent notation +5
+var y = 123e-5; // exponent -5
+
+document.write(" + : " + x + "<br>");
+document.write(" - : " + y + "<br>");
+*/
+
+var x = (0.2 * 10 + 0.1 * 10) / 10;
+
+document.write(x);
