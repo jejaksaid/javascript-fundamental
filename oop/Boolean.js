a82621caaeccbdddaf381ae26efec09fa39e19cf
jejaksaid <said@fastlearningcenter.com>
2021-02-05 22:47:05 +0700

    Boolean

--- oop/Boolean.js
+++ oop/Boolean.js
@@ -1,8 +1,14 @@
-function book(title, publisher, price) {
-    this.title = title;
-    this.publisher = publisher;
-    this.price = price;
-}
+var b1 = Boolean(100);
+var b2 = Boolean(3.14);
+var b3 = Boolean(-15);
+var b4 = Boolean("Hello");
+var b5 = Boolean('false');
+var b6 = Boolean(1 + 7 + 3.14);
 
-var newBook = new book("Python", "Python Org", 2000);
-document.write(newBook.toSource())
+document.getElementById("demo").innerHTML =
+"100 is " + b1 + "<br>" +
+"3.14 is " + b2 + "<br>" +
+"-15 is " + b3 + "<br>" +
+"Any (not empty) string is " + b4 + "<br>" +
+"Even the string 'false' is " + b5 + "<br>" +
+"Any expression (except zero) is " + b6;
