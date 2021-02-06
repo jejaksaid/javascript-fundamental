97df8705b991e528c87aafa1271971fe15e800f9
jejaksaid <said@fastlearningcenter.com>
2021-02-07 00:30:58 +0700

    Dates js

--- oop/Arrays.js
+++ oop/Arrays.js
@@ -11,6 +11,12 @@ var cars = ["BMW", "Chevrolet", "Audi"];
 document.getElementById("demo").innerHTML = cars[1];
 */
 // changing the elements
+/*
 var cars = ["BMW", "Chevrolet", "Audi"];
 cars[1] = "Toyota";
 document.getElementById("demo").innerHTML = cars[1];
+*/
+
+// arrays are object
+var person = {firstName: "Said", lastName: "Ramadhan", age: 22};
+document.getElementById("demo").innerHTML = person["firstName"], ["age"];
---
+++ oop/Date.js
@@ -0,0 +1,19 @@
+// alert current date
+/*
+var d = new Date();
+alert (d);
+
+document.getElementById("demo").innerHTML;
+*/
+
+// extract the year
+/*
+var d = new Date();
+year = d.getFullYear();
+document.getElementById("demo").innerHTML = year;
+*/
+
+// get month from date object
+var d = new Date();
+month = d.getMonth();
+document.getElementById("demo").innerHTML = month;
