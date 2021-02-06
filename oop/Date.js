dfdeee9d96893db5b4653882d59bd3155564594b
jejaksaid <said@fastlearningcenter.com>
2021-02-07 00:33:57 +0700

    date methods

--- oop/Date.js
+++ oop/Date.js
@@ -14,6 +14,13 @@ document.getElementById("demo").innerHTML = year;
 */
 
 // get month from date object
+/*
 var d = new Date();
 month = d.getMonth();
 document.getElementById("demo").innerHTML = month;
+*/
+
+// date method to set the year of a date object
+var d = new Date();
+d.setFullYear(2020);
+document.getElementById("demo").innerHTML = d;
