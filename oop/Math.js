3c3951946175a67997647db18fbd290ba9436c8f
jejaksaid <said@fastlearningcenter.com>
2021-02-07 00:52:49 +0700

    math method round number to the nearest integer

--- oop/Math.js
+++ oop/Math.js
@@ -11,3 +11,9 @@ var r = Math.max(1000000, 2000);
 document.getElementById("demo").innerHTML = r;
 */
 
+// math method to round a number to the nearest integer
+/*
+var x = Math.round(0.67);
+document.getElementById("demo").innerHTML = x;
+*/
+
