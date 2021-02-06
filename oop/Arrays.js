25bfbc8a77c3e9efb10c14dc9190abec9b15a2b2
jejaksaid <said@fastlearningcenter.com>
2021-02-07 00:02:18 +0700

    Arrays

--- oop/Arrays.js
+++ oop/Arrays.js
@@ -1,3 +1,10 @@
+/*
 var cars = ["BMW", "Chevrolet", "Audi"];
 
 document.getElementById("demo").innerHTML = cars;
+*/
+
+// access the elements of an arrays
+var cars = ["BMW", "Chevrolet", "Audi"];
+
+document.getElementById("demo").innerHTML = cars[1];
