a652472b450311855be5415ab87370797f417c95
jejaksaid <said@fastlearningcenter.com>
2021-02-07 00:08:10 +0700

    changing array

--- oop/Arrays.js
+++ oop/Arrays.js
@@ -5,6 +5,12 @@ document.getElementById("demo").innerHTML = cars;
 */
 
 // access the elements of an arrays
+/*
 var cars = ["BMW", "Chevrolet", "Audi"];
 
+document.getElementById("demo").innerHTML = cars[1];
+*/
+// changing the elements
+var cars = ["BMW", "Chevrolet", "Audi"];
+cars[1] = "Toyota";
 document.getElementById("demo").innerHTML = cars[1];
