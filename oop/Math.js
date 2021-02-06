988b162d4159676705f5e510e5bb01097078cac1
jejaksaid <said@fastlearningcenter.com>
2021-02-07 00:49:31 +0700

    Math

---
+++ oop/Math.js
@@ -0,0 +1,13 @@
+// math random number
+/*
+var r = Math.random();
+
+document.getElementById("demo").innerHTML = r;
+*/
+
+// return the largest number 
+/*
+var r = Math.max(1000000, 2000);
+document.getElementById("demo").innerHTML = r;
+*/
+
